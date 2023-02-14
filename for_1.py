980c0ebf30232a33e7173ec0f822a8a95499f6ac
EasySung <ne030715@gmail.com>
2023-02-14 12:57:46 +0900

    up easy

--- for_1.py
+++ for_1.py
@@ -18,6 +18,6 @@ number = 0
 for mark in marks: 
     number = number +1 
     if mark >= 60: 
-        print("%d번 학생은 합격입니다." % number)
+        print(f"{number}번 학생은 합격입니다.")
     else: 
-        print("%d번 학생은 불합격입니다." % number)
+        print(f"{number}번 학생은 불합격입니다.")
