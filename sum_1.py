86e6d030352c8a4f2017fdb7f22a5af6f76103cc
SeoMinKey <tjalsrl6751@gmail.com>
2023-02-14 10:44:54 +0900

    update 주석

--- sum_1.py
+++ sum_1.py
@@ -1,4 +1,19 @@
-a = 10
-b = 20
+# 대화형 인터프리터 경험 후 실행할 것
+# 사칙연산 기초
+
+a = 20
+b = 10
+
 c = a + b
+d = a - b
+e = a * b
+f = a % b
+g = a / b
+
+
 print('합:', c)
+print('차:', d)
+print('곱:', e)
+print('나머지:', f)
+print('나누기:', g)
+#print('곱:', d)
--- type_1.py
+++ type_1.py
@@ -1,3 +1,5 @@
+#각종 자료형 테스트
+
 print(7, type(7))           
 print(7.2, type(7.2))
 print(3 + 4j, type(3 + 4j))
