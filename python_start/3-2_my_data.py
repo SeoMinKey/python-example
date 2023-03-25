349d24adcd81277e6d957d30f9d9e11e9f70f99f
EasySung <124114314+EasySung@users.noreply.github.com>
2023-03-25 11:55:32 +0900

    up

--- python_start/3-2_my_data.py
+++ python_start/3-2_my_data.py
@@ -6,6 +6,13 @@ gender = '남자'
 birth_day = 3.13
 hobby = '컴퓨터 프로그래밍'
 
+print('내 이름은',name+'야.\n나는',str(age)+'살이고','남자야.\n내 생일은',str(birth_day)+'일이야.\n내 취미는',hobby+'이야.')
+
+
+
+
+
+
 print(
 f'''내 이름은 {name}야. 
 나는 {age}살이고 {gender}야. 
