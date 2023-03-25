19c992b89b1018633e827614fcf91580ead89f43
EasySung <124114314+EasySung@users.noreply.github.com>
2023-03-25 11:55:25 +0900

    up

--- python_start/3-2_my_data.py
+++ python_start/3-2_my_data.py
@@ -3,12 +3,12 @@
 name = '서민기'
 age = 23
 gender = '남자'
-birth_day = '3월 13일'
+birth_day = 3.13
 hobby = '컴퓨터 프로그래밍'
 
 print(
 f'''내 이름은 {name}야. 
 나는 {age}살이고 {gender}야. 
-내 생일은 {birth_day}이야.
+내 생일은 {birth_day}일이야.
 내 취미는 {hobby}이야.''')
 # 이건 f string 이라고 하며, 변수들을 문자열 안에 쉽게 넣도록 도와주는 기능
