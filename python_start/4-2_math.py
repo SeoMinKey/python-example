d877373982f3de2a5436b64d833f7362231a5f4d
EasySung <124114314+EasySung@users.noreply.github.com>
2023-04-08 02:15:35 +0900

    up

--- python_start/4-2_math.py
+++ python_start/4-2_math.py
@@ -1,15 +1,19 @@
-# 사각형
+# 도형의 넓이
 h = 7
 l = 3
 u = 2
+r = 6
+pi = 3.1415926535897932
 
 square = h * l
 triangle = (h * l) / 2
 trapezoid = (l + u) * h / 2
+circle = pi * r ** 2
 
 print(square)
 print(triangle)
 print(trapezoid)
+print(circle)
 
 # 2차 방정식
 a = 7
