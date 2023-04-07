bd36c3ba61449c40b7265a6cacd8456f5712dc33
EasySung <124114314+EasySung@users.noreply.github.com>
2023-04-08 02:06:33 +0900

    up

---
+++ python_start/4-2_math.py
@@ -0,0 +1,21 @@
+# 사각형
+h = 7
+l = 3
+u = 2
+
+square = h * l
+triangle = (h * l) / 2
+trapezoid = (l + u) * h / 2
+
+print(square)
+print(triangle)
+
+# 2차 방정식
+a = 7
+b = 3
+c = 9
+x = 8
+
+y = a * x ** 2 + b * x + c
+
+print(y)
