6aea372198b00cc39ddc2a57934bcc055c83581f
EasySung <124114314+EasySung@users.noreply.github.com>
2023-04-17 00:19:21 +0900

    up

---
+++ python_start/5-5_if_homework.py
@@ -0,0 +1,38 @@
+# 현금 계산기
+money = 187384
+coin = 0
+
+if money // 50000:
+    coin = money // 50000
+    print(f"오만원{coin}개")
+    money -= coin * 50000
+if money // 10000:
+    coin = money // 10000
+    print(f"만원{coin}개")
+    money -= coin * 10000
+if money // 5000:
+    coin = money // 5000
+    print(f"오천원{coin}개")
+    money -= coin * 5000
+if money // 1000:
+    coin = money // 1000
+    print(f"천원{coin}개")
+    money -= coin * 1000
+if money // 500:
+    coin = money // 500
+    print(f"오백원{coin}개")
+    money -= coin * 500
+if money // 100:
+    coin = money // 100
+    print(f"백원{coin}개")
+    money -= coin * 100
+if money // 50:
+    coin = money // 50
+    print(f"오십원{coin}개")
+    money -= coin * 50
+if money // 10:
+    coin = money // 10
+    print(f"십원{coin}개")
+    money -= coin * 10
+if money:
+    print(f"일원{money}개")
