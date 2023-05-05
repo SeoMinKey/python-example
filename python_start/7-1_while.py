182de03095f1a36d54727e792f22a83c5013bb2b
EasySung <124114314+EasySung@users.noreply.github.com>
2023-05-06 03:09:43 +0900

    up

--- python_start/7-1_while.py
+++ python_start/7-1_while.py
@@ -5,16 +5,21 @@ a = 0
 while a <= 10:
     print(a)
     a = a+1 #a =-+ 1 로 줄여쓸 수 있음
-    
-#응용 , input
-num = int(input('0부터 n까지 출력합니다. 0을 입력하면 바로 종료합니다.'))
-a = 0
-while num != 0:
-    for i in range(num+1) :
-        print(i)
-    break #while문을 빠져나가는 명령어
-        
-print('종료합니다.')
-    
 
-    
+# input함수
+n = input("정수를 입력하시오.")
+print("n:", n)
+print("n의 자료형:", type(n))
+
+# break, continue
+num = int(input('0부터 n까지 출력합니다. 0을 입력하면 바로 종료합니다.'))
+i = 0
+while num != 0:
+    i += 1
+    if i % 2 == 0:
+        continue
+    print(i)
+    if i == num:
+        break #while문을 빠져나가는 명령어
+
+print('종료합니다.')    
